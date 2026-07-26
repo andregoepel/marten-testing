6367ca540d3c66310bab31218fa1b3a9fe818aac
André Göpel <andre@andregoepel.de>
2026-07-26 14:22:59 +0800

    test: add MartenFixture smoke tests

---
+++ tests/AndreGoepel.Marten.Testing.Tests/Infrastructure/SmokeTestDocument.cs
@@ -0,0 +1,7 @@
+namespace AndreGoepel.Marten.Testing.Tests.Infrastructure;
+
+internal sealed class SmokeTestDocument
+{
+    public string Id { get; init; } = string.Empty;
+    public string Value { get; init; } = string.Empty;
+}
